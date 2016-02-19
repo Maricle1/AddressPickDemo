7c69a2fff3b353c3a63b88d9448036935b1bc6ff
张建 <zhangjian@zhangjiandemacbook-pro.local>
2016-02-19 10:16:07 +0800

    Initial Commit

---
+++ AddressPickDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddressPickDemo
+//
+//  Created by 张建 on 16/2/19.
+//  Copyright © 2016年 连藤网络科技公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AddressPickDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AddressPickDemo
+//
+//  Created by 张建 on 16/2/19.
+//  Copyright © 2016年 连藤网络科技公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
